a77d243f3ab83d220bc4da504d7f52927b2e5b06
unknown <ermiasogbazghi@gmail.com>
2019-11-07 20:49:13 -0500

    Intiial Commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        try
+        {
+            Console.WriteLine("hello");
+            //throw new Exception("Sorry ");
+            //    Console.WriteLine(DivideXByY(0, 0));
+        }catch(Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        Console.WriteLine("bye");
+    }
+
+    private static decimal DivideXByY(int x, int y)
+    {
+        return x / y;
+    }
+
+}
+
